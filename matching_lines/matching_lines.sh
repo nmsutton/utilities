3d841361e7398f714952dd3339b987abade414e0
Nate Sutton <natemsutton@gmail.com>
2020-10-12 00:53:56 -0400

    added line matching software

---
+++ matching_lines/matching_lines.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+#
+# outputs lines that don't match between two files
+# usage: $ matching_lines.sh <file1> <file2> <output_file>
+#
+
+file1="$1";
+file2="$2";
+output_file="$3";
+echo "" > $output_file;
+line_found="false";
+nonmatching="nonmatching lines: ";
+
+i=0;
+while read line1; do
+	line_found="false";
+	while read line2; do
+		if [ "$line1" = "$line2" ];
+		then
+			line_found="true";
+		fi
+	done <$file2
+
+	if [ "$line_found" = "false" ];
+	then
+		echo $line1 >> $output_file;
+		i=$(expr $i + 1);
+	fi
+done <$file1
+
+echo $nonmatching$i;
---
+++ rename_blanks/rename_blanks.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+cd $1
+apos="[']";
+rename 's/ /_/g' ./*
+rename "s/$apos//g" ./*
+rename "s/[)]//g" ./*
+rename "s/[(]//g" ./*
+rename "s/[.]com//g" ./*
+rename "s/[.]//g" ./*
+rename "s/[\.]//g" ./*
+rename "s/[&!]//g" ./*
