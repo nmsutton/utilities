c11c4d0b6f8ddb49c327a3bac4fbb5fce3c2cb28
Nate Sutton <natemsutton@gmail.com>
2021-01-30 17:23:18 -0500

    added custom folder option

--- find_videos/links_config_example.sh
+++ find_videos/links_config_example.sh
@@ -6,6 +6,8 @@ end=1000; # ending file index
 folder1= # output directory
 folder2= # input directory
 find_videos_script=/general/software/utilities/find_videos/find_videos.sh
+custom_folder_name=""; # specify a particular folder for input
+delete_new_output_folder="no"; # Remove previously created new output folder. The folder will be recreated.
 
 findmedia(){
 	command="xargs -L1 echo";
@@ -30,18 +32,33 @@ processfolder(){
 	echo \"$input_folder_name $output_folder $starting_folder\" | findmedia
 }
 
-command="ls -d $folder1/*";
-folders=$(eval $command);
-for folder in $folders
-do
-	command="echo \"$folder\" | sed 's/^.*\/\(.*\)$/\1/'";
-	folder_name=$(eval $command);
-	if [ $folder_name != "aab_groups" ] && [ $folder_name != "classic" ] && [ $folder_name != "favorites" ];
-    then
-    	if [[ $i -ge $start ]] && [[ $i -le $end ]];
-    	then
-			echo \"$folder_name\" | processfolder
-  		fi
-  		((i=i+1))
+if [ $custom_folder_name != "" ];
+then
+	if [ $delete_new_output_folder == "yes" ];
+	then
+		trash-put $folder1/$custom_folder_name/combined/
 	fi
-done
+	echo \"$custom_folder_name\" | processfolder
+	#echo \"$custom_folder_name\"
+else
+	command="ls -d $folder1/*";
+	folders=$(eval $command);
+	for folder in $folders
+	do
+		command="echo \"$folder\" | sed 's/^.*\/\(.*\)$/\1/'";
+		folder_name=$(eval $command);
+		if [ $folder_name != "aab_groups" ] && [ $folder_name != "classic" ] && [ $folder_name != "favorites" ];
+	    then
+	    	if [[ $i -ge $start ]] && [[ $i -le $end ]];
+	    	then
+	    		if [[ $delete_new_output_folder=="yes" ]];
+				then
+					trash-put $folder1/$folder_name/combined/
+				fi
+				echo \"$folder_name\" | processfolder
+				#echo \"$folder_name\" && echo "i: $i"
+	  		fi
+	  		((i=i+1))
+		fi
+	done
+fi
